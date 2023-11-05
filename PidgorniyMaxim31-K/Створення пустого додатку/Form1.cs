e892e70efd94d9e07fc077e7292ff0547002f4e3
MaximPidgorniy <149964007+MaximPidgorniy@users.noreply.github.com>
2023-11-05 22:03:07 +0300

    Add files via upload

---
+++ PidgorniyMaxim31-K/Створення пустого додатку/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("HI");
+            this.BackColor = Color.Aquamarine;
+            this.Width = 250;
+            this.Height = 250;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = Color.Aquamarine;
+            this.ForeColor = Color.Red;
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+
+        }
+    }
+}
